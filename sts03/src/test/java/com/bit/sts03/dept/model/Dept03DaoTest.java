030db90c13505b5a1ecd785fa708e5a259f9fde0
JinKim <kimjin.dev@gmail.com>
2020-09-14 12:26:24 +0900

    spring3.x - TDD기반 개발 setting -h2 connecting test3; colocar

--- sts03/src/test/java/com/bit/sts03/dept/model/Dept03DaoTest.java
+++ sts03/src/test/java/com/bit/sts03/dept/model/Dept03DaoTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -20,48 +21,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.bit.sts03.dept.model.entity.Dept03Vo;
 
 public class Dept03DaoTest {
-	Dept03Vo target = new Dept03Vo(1, "test", "test");
+	static Dept03Vo target = new Dept03Vo(1, "test", "test");
 	Dept03Dao dept03Dao;
 	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testSelectAll() {
-		//fail("Not yet implemented");
-		ApplicationContext ac=null;
-		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
-		dept03Dao=(Dept03Dao) ac.getBean("dept03Dao");
-		//assertNotNull(dept03Dao.selectAll());
-//		assertSame(1, dept03Dao.selectAll().size());
-		System.out.println(dept03Dao.selectAll().get(0));
-	}
-
-	@Test
-	public void testSelectOne() {
-		
-		ApplicationContext ac=null;
-		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
-		dept03Dao=(Dept03Dao) ac.getBean("dept03Dao");
-		assertEquals(target , dept03Dao.selectOne(target.getDeptno()));
-	}
-
-	@Test
-	public void testFrist() throws SQLException {
+		System.out.println("setUpBeforeClass");
+//		String sql="CREATE TABLE IF NOT EXISTS `dept03` (" + 
+		String sql="CREATE TABLE `dept03` (" + 
+				"	`deptno` INT NOT NULL AUTO_INCREMENT," + 
+				"	`dname` VARCHAR(10) NULL DEFAULT NULL," + 
+				"	`loc` VARCHAR(10) NULL DEFAULT NULL," + 
+				"	PRIMARY KEY (`deptno`)" + 
+				")";
 		ApplicationContext ac=null;
 		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
 		//DataSource dataSource=(DataSource) ac.getBean("dataSource");
@@ -72,13 +45,50 @@ public class Dept03DaoTest {
 		DataSource dataSource=(DataSource) ac.getBean("dataSource");
 		Connection conn=dataSource.getConnection();
 		/* create table */
-		conn.prepareStatement("CREATE TABLE IF NOT EXISTS `dept03` (" + 
-				"	`deptno` INT NOT NULL AUTO_INCREMENT," + 
-				"	`dname` VARCHAR(10) NULL DEFAULT NULL," + 
-				"	`loc` VARCHAR(10) NULL DEFAULT NULL," + 
-				"	PRIMARY KEY (`deptno`)" + 
-				")").execute();
+		conn.prepareStatement(sql).execute();
 		/* insert dummy data */
-		conn.prepareStatement("insert into dept03 (dname,loc) values ('"+target.getDname()+"','"+target.getLoc()+"')").executeUpdate();
+		conn.prepareStatement("insert into dept03 (dname,loc) "
+				+ "values ('"+target.getDname()+"','"+target.getLoc()+"')").executeUpdate();
 	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("tearDownAfterClass");
+		File file = new File("c:\\data\\xeTest.mv.db");
+		if(file.exists())file.deleteOnExit();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setUp");
+		ApplicationContext ac=null;
+		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
+		dept03Dao=(Dept03Dao) ac.getBean("dept03Dao");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("tearDown");
+		
+	}
+
+	@Test
+	public void testSelectAll() {
+		//fail("Not yet implemented");
+
+		
+		//assertNotNull(dept03Dao.selectAll());
+//		assertSame(1, dept03Dao.selectAll().size());
+		System.out.println(dept03Dao.selectAll().get(0));
+	}
+
+	@Test
+	public void testSelectOne() {
+		
+
+		
+		assertEquals(target , dept03Dao.selectOne(target.getDeptno()));
+	}
+
+
 }
