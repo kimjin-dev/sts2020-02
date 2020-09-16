528f9be55eb66b985e18e70d7c18496df81784ca
JinKim <kimjin.dev@gmail.com>
2020-09-16 12:12:32 +0900

    mybatis test insert&delete

--- sts05/src/test/java/com/bit/sts05/TestContext.java
+++ sts05/src/test/java/com/bit/sts05/TestContext.java
@@ -1,11 +1,16 @@
 package com.bit.sts05;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.sql.DataSource;
 
@@ -39,18 +44,50 @@ public class TestContext {
 		public void testSqlSessionFactory() {
 			assertNotNull(sqlSessionFactory);
 		}
-		
+
+		/*
+		 * @Test public void testSelectDept() { try( SqlSession session
+		 * =sqlSessionFactory.openSession() ){ List<Map<String, String>>
+		 * list=session.selectList("dept.selectDept"); assertNotNull(list);
+		 * for(Map<String,String>map:list) { log.debug(map.toString()); } }
+		 * 
+		 * }
+		 */
 		@Test
 		public void testSelectDept() {
 			try(
-				SqlSession session =sqlSessionFactory.openSession()
-					){
-				List<Map<String, String>> list=session.selectList("dept.selectDept");
-				assertNotNull(list);
-				for(Map<String,String>map:list) {
-					log.debug(map.toString());
+					SqlSession session =sqlSessionFactory.openSession()
+						){
+					Map<String, Object> map =session.selectOne("dept.selectDept",1234);
+					Set<Entry<String,Object>> entrySet=map.entrySet();
+					Iterator<Entry<String, Object>> ite= entrySet.iterator();
+					while(ite.hasNext()) {
+						Entry<String, Object> entry = ite.next();
+						log.debug(entry.getKey()+":"+entry.getValue());
+					}
 				}
-			}
+		}
 		
+		@Test
+		public void testInsertTest() {
+			try(
+					SqlSession session = sqlSessionFactory.openSession();	
+					){
+				Map<String, Object> map=new HashMap<String, Object>();
+				map.put("dname", "tester");
+				map.put("loc", "ÇÐ¿ø");
+				map.put("deptno",1234);
+				session.insert("insertTest",map);
+			}
+		}
+		
+		@Test
+		public void testDeleteTest() {
+			try(
+					SqlSession session = sqlSessionFactory.openSession();
+					){
+				assertSame(1, session.delete("deleteTest",1234));
+				
+			}
 		}
 }
