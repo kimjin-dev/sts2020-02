2664018407d5d6f667ca90918b0628419b5d52fe
JinKim <kimjin.dev@gmail.com>
2020-09-21 15:53:06 +0900

    gson

--- sts08/src/main/java/com/bit/sts08/DeptCountroller.java
+++ sts08/src/main/java/com/bit/sts08/DeptCountroller.java
@@ -1,6 +1,8 @@
 package com.bit.sts08;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -8,22 +10,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bit.sts08.service.DeptService;
+import com.google.gson.Gson;
 
 @Controller
 public class DeptCountroller {
 		@Inject
 		DeptService deptService;
 		
-		@RequestMapping(value = "/dept/", method=RequestMethod.GET)
-		public String listPage(Model model) {
+		@RequestMapping(value = "/dept/", method=RequestMethod.GET, produces = {"application/json; charset=utf-8"})
+		public @ResponseBody String listPage(Model model) {
 			try {
 				deptService.list(model);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			return "listJson";
+			Map map=new HashMap();
+			map.put("root", model.asMap().get("list"));	
+			
+			//java-<json변환 toString
+			Gson gson = new Gson();
+			return gson.toJson(map);
 		}
 		
 }
