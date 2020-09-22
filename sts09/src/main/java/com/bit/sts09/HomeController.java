58440dc4768a4aced76b52df53f6d64a14e701d6
JinKim <kimjin.dev@gmail.com>
2020-09-22 11:37:07 +0900

    fileUpload

--- sts09/src/main/java/com/bit/sts09/HomeController.java
+++ sts09/src/main/java/com/bit/sts09/HomeController.java
@@ -1,16 +1,20 @@
 package com.bit.sts09;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class HomeController {
+	String path="C:\\Jworkspace\\sts2020-02\\upload\\";
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -32,31 +37,49 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/upload", method = RequestMethod.POST)
-	@ResponseBody
-	public void upload(@RequestParam("sabun") int sabun, MultipartFile file1) {
-		logger.info("sabun="+sabun);
-		logger.info("file:"+file1.getOriginalFilename());
-		String path="C:\\Jworkspace\\sts2020-02\\upload\\";
-		File file=null;
-		file = new File(path+file1.getOriginalFilename());
-		InputStream is= null;
-		OutputStream os=null;
+	public String upload(@RequestParam("sabun") int sabun, MultipartFile file1, Model model) {
+		String orgName=file1.getOriginalFilename();
+		String reName=System.currentTimeMillis()+"_"+orgName;
+		
+		File dest = new File(path+reName);
+		
 		try {
-			is=file1.getInputStream();
-			os=new FileOutputStream(file);
-			int su=-1;
-			while((su=is.read())!=-1) {
-				os.write(su);
-			}
+			file1.transferTo(dest);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		}
+		model.addAttribute("name", reName);
+		return "download";
+		
+	}
+	
+	@RequestMapping("/download/{name:.+}")// 확장자 포함 pathvariable
+	public void download(@PathVariable String name, HttpServletResponse res) {
+		logger.info(name);
+		File file=new File(path+name);
+		if(file.exists()) {
+			OutputStream os=null;
+			InputStream is=null;
+			res.setContentType("application/octet-stream");
+			try {
+				os=res.getOutputStream();
+				is=new FileInputStream(file);
+				int su=-1;
+				while((su=is.read())!=-1) {
+					os.write(su);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
 				try {
-					if(is!=null)is.close();
 					if(os!=null)os.close();
+					if(is!=null)is.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+			}
 		}
 	}
 }
