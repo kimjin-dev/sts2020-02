cb2c85cf16c94eea40dd256549b2eb75a6869a5c
JinKim <kimjin.dev@gmail.com>
2020-09-23 11:17:02 +0900

    WebSocket - chet1

--- sts11/src/main/java/com/bit/sts11/HomeController.java
+++ sts11/src/main/java/com/bit/sts11/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -17,8 +19,8 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		
+	public String home(HttpSession session, String id) {
+		session.setAttribute("id", id);
 		return "home";
 	}
 	
--- sts11/src/main/java/com/bit/sts11/websocket/ChattingHandler.java
+++ sts11/src/main/java/com/bit/sts11/websocket/ChattingHandler.java
@@ -1,25 +1,31 @@
 package com.bit.sts11.websocket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class ChattingHandler extends TextWebSocketHandler {
+	static List<WebSocketSession> list = new ArrayList<>();
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		session.sendMessage(new TextMessage("입장하셨습니다"));
+		list.add(session);
 	}
 	
 	@Override
-	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-		session.sendMessage(message);
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		System.out.println(message.getPayloadLength());
+		for(WebSocketSession sock: list)
+			sock.sendMessage(message);
 	}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-
+		list.remove(session);
 	}
 }
